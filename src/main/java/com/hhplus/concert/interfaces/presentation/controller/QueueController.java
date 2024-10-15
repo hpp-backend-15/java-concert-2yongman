b6bf9b317e8a989b1b52727413f19ababf222266
이용만 <dydaks7878@gmail.com>
2024-10-15 20:28:27 +0900

    [fix]QueueController 수정

--- src/main/java/com/hhplus/concert/interfaces/presentation/controller/QueueController.java
+++ src/main/java/com/hhplus/concert/interfaces/presentation/controller/QueueController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-public class WaitingTokenController {
+public class QueueController {
 
     @PostMapping("/token")
     public ResponseEntity<TokenResponseDto> createToken(@RequestBody TokenRequestDto dto){
